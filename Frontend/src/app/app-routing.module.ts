fe50e4055e0160022816ee576bb480e01ac70c31
Fagyesz <vinczef.o@gmail.com>
2022-11-13 22:53:59 +0000

    Pages

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { RegisterComponent } from './page/register/register.component';
+import { ErrorComponent } from './page/error/error.component';
+import { ProfileComponent } from './page/profile/profile.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { AboutComponent } from './page/about/about.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},
+{
+  path: 'profile',
+  component: ProfileComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: '**',
+  component: ErrorComponent
+}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
